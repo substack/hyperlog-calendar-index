07974ac5390ffc73535fe60d9cef326637295dd0
James Halliday <mail@substack.net>
2016-05-24 11:13:33 +0200

    example mostly works

--- example/events.js
+++ example/events.js
@@ -1,5 +1,6 @@
 var hyperlog = require('hyperlog')
 var memdb = require('memdb')
+var xtend = require('xtend')
 var cali = require('../')
 
 var log = hyperlog(memdb(), { valueEncoding: 'json' })
@@ -7,27 +8,27 @@ var cal = cali({
   log: log,
   db: memdb(),
   map: function (row) {
-    if (row.value.type === 'cal') return row.value.time
+    return xtend(row, {
+      type: 'put',
+      time: row.value.time,
+      value: { title: row.value.title }
+    })
   }
 })
 log.add(null, {
-  type: 'cal',
   title: 'javascript study group',
   time: 'every thursday at 7pm'
 })
 log.add(null, {
-  type: 'cal',
   title: 'hardware hack night',
   time: 'every tuesday at 7pm'
 })
 log.add(null, {
-  type: 'cal',
   title: 'cyberwizard institute',
   time: 'every day at 12:00 starting jan 15 until feb 10'
-  //time: 'mon-fri 12:00-17:00 starting jan 5 until jan 30'
 })
 
-cal.createReadStream({
-  gt: new Date('2016-01-01'),
-  lt: new Date('2016-01-31')
+cal.query({
+  gt: '2016-01-01',
+  lt: '2016-01-31'
 }).on('data', console.log)
--- index.js
+++ index.js
@@ -1,56 +1,61 @@
 var hindex = require('hyperlog-index')
+var calendar = require('calendar-db')
 var sub = require('subleveldown')
-var parse = require('parse-messy-schedule')
-var through = require('through2')
-var lexi = require('lexicographic-integer')
-var readonly = require('read-only-stream')
+var duplexify = require('duplexify')
+var once = require('once')
+
+var DEX = 'd', CAL = 'c'
 
 module.exports = Cal
 
 function Cal (opts) {
   if (!(this instanceof Cal)) return new Cal(opts)
-  var self = this
-  self.db = opts.db
-  self.xdb = sub(self.db, 'x')
-  self.log = opts.log
-  self.map = opts.map
-  self.ix = hindex(self.log, sub(self.db, 'i'), function (row, next) {
-    var time = self.map(row)
-    if (!time) return next()
-    var p = parse(time)
-    var a = p.range[0].getTime(), b = p.range[0].getTime()
-    var pa = Math.abs(a), pb = Math.abs(b)
-    var xa = a < 0 ? '0' : '1'
-    var xb = b < 0 ? '0' : '1'
-
-    var lowerKey = 'lower!' + encodeDate(p.range[0]) + '!' + row.key
-    var upperKey = 'upper!' + encodeDate(p.range[1]) + '!' + row.key
-    self.xdb.batch([
-      { type: 'put', key: upperKey, value: 0 },
-      { type: 'put', key: lowerKey, value: 0 }
-    ], next)
+  var cal = this.cal = calendar(sub(opts.db, CAL))
+  var map = opts.map
+  this.dex = hindex({
+    log: opts.log,
+    db: sub(opts.db, DEX),
+    map: function (row, next) {
+      next = once(next)
+      var doc = opts.map(row)
+      if (!doc) return next()
+      var time = doc.time
+      var batch = []
+      var pending = 1 + row.links.length
+      row.links.forEach(function (link) {
+        cal.get(link, function (err, ldoc) {
+          if (err) return next(err)
+          batch.push.apply(batch, cal.prepare(ldoc.time, {
+            type: 'del',
+            id: link,
+            created: ldoc.created
+          }).batch)
+          if (--pending === 0) done()
+        })
+      })
+      if (--pending === 0) done()
+      function done () {
+        batch.push.apply(batch, cal.prepare(doc.time, {
+          type: 'put',
+          id: row.key,
+          created: doc.created,
+          value: doc.value
+        }).batch)
+        cal.db.batch(batch, next)
+      }
+    }
   })
 }
 
-Cal.prototype.createReadStream = function (opts) {
-  // an interval or segment tree would be more appropriate
-  var self = this
-  var stream = through.obj(write)
-  self.ix.ready(function () {
-    self.xdb.createReadStream({
-      gt: opts.gt ? 'lower!' + encodeDate(opts.gt) : undefined,
-      lt: opts.lt ? 'lower!' + encodeDate(opts.lt) : undefined
-    }).pipe(stream)
-  })
-  return readonly(stream)
-
-  function write (row, enc, next) {
-    var k = row.key.split('!')[2]
-    self.log.get(k, next)
-  }
+Cal.prototype.ready = function (fn) {
+  this.dex.ready(fn)
 }
 
-function encodeDate (d) {
-  var x = Math.abs(d.getTime())
-  return (x < 0 ? '0' : '1') + Buffer(lexi.pack(x)).toString('hex')
+Cal.prototype.query = function (opts, cb) {
+  var self = this
+  var d = duplexify.obj()
+  self.ready(function () {
+    d.setReadable(self.cal.query(opts, cb))
+  })
+  return d
 }
